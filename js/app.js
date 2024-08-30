e9bf2cad2683b46599bc571863f5e36dc9ad4e6a
Caique Salmaso <caiquesalmaso@gmail.com>
2024-08-30 18:03:38 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SobreIFSP from './SobreIFSP'; // Importa o componente React que você criou
+
+function App() {
+  return (
+    <div>
+      <SobreIFSP /> {/* Exibe o novo componente */}
+    </div>
+  );
+}
+
+export default App;
---
+++ js/sobreIFSP.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const SobreIFSP = () => {
+  return (
+    <div>
+      <header id="cabecalho"> {/* Indica o cabeçalho do nosso documento */}
+        <nav>
+          <object width="100%" height="200px" data="menu.html"></object>
+        </nav>
+      </header>
+      
+      <main>
+        <p><strong>O Instituto Federal de Educação, Ciência e Tecnologia de São Paulo</strong> – é uma autarquia federal de ensino.</p>
+        <p>Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.</p>
+        <p>Durante seus 100 anos de história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.</p>
+        <p>Com a mudança, o IFSP destinará 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.</p>
+        <p>Além dos cursos presenciais, o Instituto Federal de São Paulo oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).</p>
+        <p><strong>O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuídas pelo estado de São Paulo.</strong></p>
+        <p><strong>Localizado em Rua Pedro Vicente, 625 - Canindé - São Paulo - SP - Brasil - Cep: 01109-010</strong></p>
+      </main>
+      
+      <footer>
+        <object width="100%" height="200px" data="rodape.html"></object>
+      </footer>
+    </div>
+  );
+};
+
+export default SobreIFSP;
